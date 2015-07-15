7a682d165949b2aa05d2e399f6f34e1453773d06
xinkong <2624384463@qq.com>
2015-07-15 14:22:25 +0800

    加如注册页面

--- app/src/main/java/com/example/zch/imspeak/ui/login/RegisterActivity.java
+++ app/src/main/java/com/example/zch/imspeak/ui/login/RegisterActivity.java
@@ -9,58 +9,47 @@ import android.widget.TextView;
 
 import com.example.zch.imspeak.R;
 import com.example.zch.imspeak.base.BaseActivity;
-import com.example.zch.imspeak.ui.MainActivity;
 import com.lidroid.xutils.ViewUtils;
 import com.lidroid.xutils.view.annotation.ViewInject;
 
-public class LoginActivity extends BaseActivity implements View.OnClickListener{
+public class RegisterActivity extends BaseActivity implements View.OnClickListener{
 
-    @ViewInject(R.id.login_et_name)
+    @ViewInject(R.id.register_et_name)
     private EditText mEtName;
-    @ViewInject(R.id.login_et_pwd)
+    @ViewInject(R.id.register_et_Pwd)
     private EditText mEtPwd;
-    @ViewInject(R.id.login_btn_ok)
+    @ViewInject(R.id.register_et_againPwd)
+    private EditText mEtAgainPwd;
+    @ViewInject(R.id.register_btn_ok)
     private Button mBtnOk;
-    @ViewInject(R.id.login_tv_register)
-    private TextView mTvRegister;
-    @ViewInject(R.id.login_tv_forgetPwd)
-    private TextView mTvForgetPwd;
 
     @Override
     public int getlayoutResID() {
-        return R.layout.activity_login;
+        return R.layout.activity_register;
     }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         ViewUtils.inject(this);
-
         init();
+    }
 
+    private void init() {
+        mBtnOk.setOnClickListener(this);
     }
 
     @Override
     protected void initHeadView(LinearLayout ll_headview, TextView tv_left, TextView tv_title, TextView tv_right) {
         super.initHeadView(ll_headview, tv_left, tv_title, tv_right);
-        tv_title.setText("登录");
-    }
-
-    private void init() {
-        mBtnOk.setOnClickListener(this);
-        mTvRegister.setOnClickListener(this);
-        mTvForgetPwd.setOnClickListener(this);
+        tv_title.setText("用户注册");
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.login_btn_ok:
-                toActivity(MainActivity.class);
-                break;
-            case R.id.login_tv_register:
-                toActivity(RegisterActivity.class);
+            case R.id.register_btn_ok:
+                //注册成功,执行注册逻辑
+                show("注册成功了");
                 break;
         }
     }
